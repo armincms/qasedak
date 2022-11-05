3a06c19158a7549a3e8f12c79e818627be988b00
Ismail Zare <zarehesmaiel@gmail.com>
2022-11-05 08:40:15 +0330

    wipe

--- resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  router.addRoutes([
-    {
-      name: 'qasedak',
-      path: '/qasedak',
-      component: require('./components/Tool'),
-    },
-  ])
-})
